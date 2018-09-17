566f614b82952dce3af9bd8054ae719c8b7ed5ea
Viglietti Matthieu <vigliettimatthieu@gmail.com>
2018-09-17 11:20:46 +0200

    Add Root, Controller and Models Files (Posts, Comments and Manager). Database connection is setup in Manager. Writing classes for every file.

---
+++ Model/backend/Manager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MV\Blog;
+
+class Manager
+{
+	protected function dbConnect()
+	{
+		$db = new PDO(('mysql:host=localhost;dbname=blog_jf;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        return $db;)
+	}
+
+}
---
+++ Model/backend/PostManager.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace MV\Blog;
+
+class PostManager
+{
+	
+}
